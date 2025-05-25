f598ca086f78a93e3c77adb6e2241c9116aa9c79
Akash-Kamaliya <akashkamaliya112@gmail.com>
2025-05-25 17:55:37 +0530

    add Some programs

---
+++ Topic Wise Exaple/4String/StringPalindrom.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class StringPalindrom {
+	public static boolean isPalindrom(String str) {
+		for(int i = 0; i < str.length()/2;i++){
+			int n = str.length();
+			if(str.charAt(i)!=str.charAt(n-i-1)){
+				//not a palindrom
+				System.out.println("String is Not Palindrom");
+				return false;
+			}
+		}
+		System.out.println("String is Palindrom");
+		return true;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your String");
+		String name = sc.next();
+		isPalindrom(name);
+	}
+}
